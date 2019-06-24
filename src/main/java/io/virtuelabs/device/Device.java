db20cffb4c2ae41a17fd10e4a0f94ad31d6ff73b
Sangram Reddy <reddy.horcrux@gmail.com>
2019-06-24 18:51:39 +0100

    Added necessary logging

--- src/main/java/io/virtuelabs/device/Device.java
+++ src/main/java/io/virtuelabs/device/Device.java
@@ -5,20 +5,26 @@ import io.grpc.ServerBuilder;
 import io.virtuelabs.device.impl.DeviceServiceImpl;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Device {
+
+    private static final Logger LOGGER = Logger.getLogger(Device.class.getName());
+
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println("Preparing the gRPC server for start up");
+
+        LOGGER.log(Level.INFO, "Preparing the DEVICE gRPC server for start up");
         Server server = ServerBuilder.forPort(10001)
                 .addService(new DeviceServiceImpl())
                 .build();
-
+        LOGGER.log(Level.INFO, "Starting DEVICE gRPC server");
         server.start();
-
+        LOGGER.log(Level.INFO, "Successfully started DEVICE gRPC server and is running on port 10001");
         Runtime.getRuntime().addShutdownHook( new Thread ( () -> {
-            System.out.println("Received Shutdown Request");
+            LOGGER.log(Level.INFO,"Received Shutdown Request");
             server.shutdown();
-            System.out.println("Sucessfully stopped the server");
+            LOGGER.log(Level.INFO,"Sucessfully stopped DEVICE gRPC server");
         }));
 
         server.awaitTermination();
--- src/main/java/io/virtuelabs/device/impl/DeviceServiceImpl.java
+++ src/main/java/io/virtuelabs/device/impl/DeviceServiceImpl.java
@@ -1,23 +1,36 @@
 package io.virtuelabs.device.impl;
 
+
 import io.grpc.stub.StreamObserver;
 import io.virtuelabs.contract.DeviceRequest;
 import io.virtuelabs.contract.DeviceResponse;
 import io.virtuelabs.contract.DeviceServiceGrpc;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DeviceServiceImpl extends DeviceServiceGrpc.DeviceServiceImplBase {
+
+  private static final Logger LOGGER = Logger.getLogger(DeviceServiceImpl.class.getName());
 
   @Override
   public void sendEvent(DeviceRequest request, StreamObserver<DeviceResponse> responseObserver) {
 
-    System.out.println("Incoming request");
-    responseObserver.onNext(
-      DeviceResponse.newBuilder()
-        .setAnomaly(true)
-        .setMessage("Test Response")
-        .build()
-    );
-    System.out.println("Sending response");
+    LOGGER.log(Level.INFO, "Received request form device: {0}", request.getDeviceId());
+    try {
+      LOGGER.log(Level.INFO, "Processing request form device: {0}", request.getDeviceId());
+      responseObserver.onNext(
+        DeviceResponse.newBuilder()
+          .setAnomaly(true)
+          .setMessage("Test Response")
+          .build()
+      );
+    } catch (RuntimeException e) {
+      responseObserver.onError(e);
+      throw e;
+    }
+
+    LOGGER.log(Level.INFO, "Responding to request form device: {0}", request.getDeviceId());
     responseObserver.onCompleted();
   }
 }
