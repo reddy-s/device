43a64377e4cf05270d7a2fad8953bc5b3bc3a66f
Sangram Reddy <reddy.horcrux@gmail.com>
2019-06-28 16:30:20 +0100

    Adding timeout to config service

--- src/main/java/io/virtuelabs/device/client/ConfigurationClient.java
+++ src/main/java/io/virtuelabs/device/client/ConfigurationClient.java
@@ -6,10 +6,16 @@ import io.virtuelabs.contract.ConfigurationServiceGrpc;
 import io.virtuelabs.contract.DeviceRequest;
 import io.virtuelabs.device.config.DeviceChannelConfig;
 
+import java.util.concurrent.TimeUnit;
+
 public class ConfigurationClient {
 
+  private static long timeoutMs = Long.parseLong(System.getenv("CONFIGURATION_SERVICE_REQUEST_TIMEOUT"));
+
   public static ConfigResponse fetchDeviceConfig(DeviceRequest deviceRequest) {
-    ConfigurationServiceGrpc.ConfigurationServiceBlockingStub configStub = ConfigurationServiceGrpc.newBlockingStub(DeviceChannelConfig.getConfigurationManagedChannel());
+    ConfigurationServiceGrpc.ConfigurationServiceBlockingStub configStub = ConfigurationServiceGrpc
+      .newBlockingStub(DeviceChannelConfig.getConfigurationManagedChannel())
+      .withDeadlineAfter(timeoutMs, TimeUnit.MILLISECONDS);
     return configStub
       .sendEvent(ConfigRequest
         .newBuilder()
